83ae8d69d3d2a9411d9de3a66c82d28f99e158f9
Zbynek Pelunek <34337408+ZbynekPelunek@users.noreply.github.com>
2021-02-12 03:11:24 +0100

    Add selectRandomItem() function, update final message

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const createMessage = () => {
 
-    const items = ['staff', 'sword', 'shield'];
-    const creatures = ['dragon', 'corpse', 'dwarf'];
-    const classes = ['warrior', 'hunter', 'mage'];
+    const items = ['staff', 'sword', 'shield', 'flail'];
+    const creatures = ['a dragon', 'a corpse', 'a dwarf', 'an elf', 'a human', 'an undead'];
+    const classes = ['Warrior', 'Hunter', 'Mage', 'Rogue', 'Death Knight'];
+    const quality = ['a Common', 'an Uncommon', 'a Rare', 'an Epic', 'a Legendary']
 
-    console.log(`You looted ${items[0]} from ${creatures[0]} for a ${classes[0]}.`);
+    console.log(`Your ${selectRandomItem(classes)} looted ${selectRandomItem(quality)} ${selectRandomItem(items)} from ${selectRandomItem(creatures)}.`);
+};
+
+const selectRandomItem = arrOfItems => {
+
+    return arrOfItems[Math.floor(Math.random() * arrOfItems.length)];
 };
 
 createMessage();
